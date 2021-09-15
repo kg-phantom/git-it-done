1a6f101a30e091b14a432453eaedd19dca6a1546
Kyrsten <ksurbayan@gmail.com>
2021-09-14 16:21:55 -0800

    error handling by redirecting to homepage

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,22 @@
+var repoNameEl = document.querySelector("#repo-name");
+var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        // if no repo was given, redirect to homepage
+        document.location.replace("./index.html");
+    }
+    
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,9 +26,14 @@ var getRepoIssues = function(repo) {
         if(response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if(response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
@@ -49,4 +72,17 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues("kg-phantom/code-quiz");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
+
+//getRepoIssues();
